9bb2f9ef1f70754a5cbc9e030475866dcd4f1dea
ahnafhemal <hemalhasan140@gmail.com>
2023-03-25 22:25:59 +0600

    display comment

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ callPost()
 const displayPost = posts => {
     const mainDiv = document.getElementById('all-posts')
     for (const post of posts) {
-        console.log(post)
+        // console.log(post)
         const div = document.createElement('div')
         div.classList.add('col')
 
@@ -19,11 +19,29 @@ const displayPost = posts => {
            <div class="card-body">
             <h5 class="card-title">${post.id}</h5>
             <p class="card-text">${post.title}</p>
-            <button class="btn btn-primary">Details</button>
+            <button onclick="commentDetails('${post.id}')" class="btn btn-primary">Details</button>
            </div>
         </div>
         `
 
         mainDiv.appendChild(div)
     }
+}
+
+
+
+const commentDetails = details => {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayComment(data[0]))
+}
+
+
+const displayComment = Comment => {
+    const commentBox = document.getElementById('comment')
+    const h6 = document.createElement('h6')
+    h6.innerText = `${Comment.body}`
+    commentBox.appendChild(h6)
+
 }
